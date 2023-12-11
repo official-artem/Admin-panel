5fd64378f17af51bfcc21e72777000c87896ca6c
official-artem <grechuck.artem.ag@gmail.com>
2023-12-11 12:32:08 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <span>Admin panel</span>
+
+      <a href='/'>Create new user</a>
+    </header>
+  )
+}
